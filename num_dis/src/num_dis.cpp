1a57d7f32191d0489ef8ce443feb0b91bccf591b
lbw020816 <85294126+lbw020816@users.noreply.github.com>
2022-01-21 17:17:27 +0800

    Add files via upload

---
+++ num_dis/src/num_dis.cpp
@@ -0,0 +1,26 @@
+#include<ros/ros.h>
+#include <opencv2/opencv.hpp>
+#include<cv_bridge/cv_bridge.h>
+#include<ros/ros.h>
+using namespace std;
+void imageCallback(const sensor_msgs::ImagePtr& msg);
+
+int main(int argc,char **argv)
+{
+    ros::init(argc, argv, "num_dis");
+    ros::start();
+    ros::NodeHandle n;
+    ros::Subscriber imageSub = n.subscribe("/armor_detector/armor_roi",1,&imageCallback);
+    cv::namedWindow("num_dis");
+    ros::spin();
+
+    return 0;
+}
+
+void imageCallback(const sensor_msgs::ImagePtr& msg)
+{
+    ROS_INFO("aaa");
+    cv::Mat img =cv_bridge::toCvCopy(*msg, sensor_msgs::image_encodings::BGR8)->image;
+    cv::imshow("num_dis",img);
+    cv::waitKey(1);
+}
