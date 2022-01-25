c9e1af3f68d202f28443b13de7022c3ab8f11953
lbw <1908127172>
2022-01-26 01:07:59 +0800

    try but failed

--- num_dis/src/num_dis.cpp
+++ num_dis/src/num_dis.cpp
@@ -1,9 +1,10 @@
 #include<ros/ros.h>
 #include <opencv2/opencv.hpp>
 #include<cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
 #include<ros/ros.h>
 using namespace std;
-void imageCallback(const sensor_msgs::ImagePtr& msg);
+void imageCallback(const sensor_msgs::ImageConstPtr& msg);
 
 int lastTime;
 int num;
@@ -13,14 +14,15 @@ int main(int argc,char **argv)
     ros::init(argc, argv, "num_dis");
     ros::start();
     ros::NodeHandle n;
-    ros::Subscriber imageSub = n.subscribe("/armor_detector/armor_num",1,&imageCallback);
+    image_transport::ImageTransport it(n);
+    image_transport::Subscriber imageSub =it.subscribe("/armor_detector/armor_num",1,imageCallback);
     cv::namedWindow("num_dis");
     lastTime = ros::Time::now().toSec();
     ros::spin();
     return 0;
 }
 
-void imageCallback(const sensor_msgs::ImagePtr& msg)
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
 
     cv::Mat special;
